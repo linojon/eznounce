979964689fab55ac973085db44bbcd3dad571bb5
Jonathan Linowes <linojon@gmail.com>
2013-10-14 14:20:58 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Eznounce::Application.config.secret_key_base = '6162cc542dbbe29d196884edfa139b04e41abc2d2883d467908bfc145522785a107acb2609439e1f872b6b27879e80dfd15b891f88da405f4ec4e8f0ce853727'
